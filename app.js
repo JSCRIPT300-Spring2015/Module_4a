82d2c431a385f12fd50f1ca4a4f0d4ed2c4e9677
Bernie Macias <bmacias@httpstergeek.com>
2015-04-25 21:21:17 -0700

    add Rest methods

--- app.js
+++ app.js
@@ -2,3 +2,28 @@
 //  to serve up static files from the public directory (index.html and client.js - you 
 //  can also serve up css files from the public directory this way if you want)
 //  you need to support a '/trucks' endpoint, and a dynamic route for '/trucks:name'
+var express = require('express');
+var trucks = require('./trucks');
+var app = express();
+
+app.get('/', function (request, response) {
+
+  // redirect to /food-trucks instead
+  response.redirect('/trucks');
+});
+
+app.get('/trucks', function (request, response) {
+  response.json(trucks.getTrucks());
+});
+
+app.get('/trucks/:name', function (request, response) {
+  response.json(trucks.getTruck(request.params.name));
+});
+
+app.get('/food-types', function (request, response) {
+  response.json(trucks.getFoodTypes());
+});
+
+app.listen(3000, function () {
+  console.log('listening on port 3000');
+});

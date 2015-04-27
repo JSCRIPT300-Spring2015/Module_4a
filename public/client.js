9b53bb9daaebd86bbd2db96520cd2e41700c040d
Josh Lin <joshlin@uw.edu>
2015-04-26 22:55:19 -0700

    first commit for module 4

--- public/client.js
+++ public/client.js
@@ -1,9 +1,19 @@
-// $(function () {
-// 	$.get('/trucks', function (trucks) {
-// 		var truckList = [];
-// 		trucks.forEach(function (truck) {
-// 			truckList.push('<li><a href="/trucks/' + truck.name + '/">' + truck.name + '</a></li>')
-// 		});
-// 		$('.truck-list').append(truckList);
-// 	});
-// });
+$(function () {
+	
+	$.get('/trucks', function (trucks) {
+		var truckList = [];
+		trucks.forEach(function (truck) {
+			truckList.push('<li><a href="/trucks/' + truck.name + '/">' + truck.name + '</a></li>')
+		});
+		$('.truck-list').append(truckList);
+	});
+
+	$.get('/food-types', function (foodTypes) {
+		var foodTypeList = [];
+		foodTypes.forEach(function (type) {
+			foodTypeList.push('<li><a href="/food-types/' + type + '/">' + type + '</a></li>')
+		});
+		$('.food-type-list').append(foodTypeList);
+	});	
+
+});

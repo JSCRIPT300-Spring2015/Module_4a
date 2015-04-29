de58e47cc078ed936cc7103b27397e8bd05565e0
jamdon <jamdon@uw.edu>
2015-04-28 18:56:20 -0700

    Update app.js
    fixed example route syntax

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 //  in this file create an express application - use the middle-ware built into express
 //  to serve up static files from the public directory (index.html and client.js - you 
 //  can also serve up css files from the public directory this way if you want)
-//  you need to support a '/trucks' endpoint, and a dynamic route for '/trucks:name'
+//  you need to support a '/trucks' endpoint, and a dynamic route for '/trucks/:name'

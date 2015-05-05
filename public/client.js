20dd7562136b06d04fa769abb82dbdd95795a561
Sam N <segid_s@yahoo.com>
2015-05-04 20:51:15 -0700

    commenting out none working code

--- public/client.js
+++ public/client.js
@@ -6,21 +6,25 @@
  		});
  		$('.truck-list').append(truckList);
  	});
-         
-    $('#myDiv').click(function() {
-        $('.truck-list').hide();
-        
-        $.ajax({
-            method: 'GET',
-            url: '/food-types',
-            dataType: JSON,
-        })
-            .done(function (foodArray) {
-                var foodList = [];
-                foodArray.forEach(function (food) {
-                    foodList.push('<li><a href="/food-types/' + food + '/">' + food + '</a></li>')
-                });
-                $('.food-list').append(foodList);
-            });
-    });
+     
+     
+//     i tried the extra credit as follow, but it's not working
+     
+//    $('#myDiv').click(function() {
+//        $('.truck-list').hide();
+//        
+//        $.ajax({
+//            method: 'GET',
+//            url: '/food-types',
+//            dataType: JSON,
+//        })
+//            .done(function (foodArray) {
+//                var foodList = [];
+//                foodArray.forEach(function (food) {
+//                    foodList.push('<li><a href="/food-types/' + food + '/">' + food + '</a></li>')
+//                });
+//                $('.food-list').append(foodList);
+//            });
+//    });
+     
 });

1531e0aa8b5797984e411578f90a9188819be40b
Ted <tedmmc@uw.edu>
2015-05-05 09:17:06 -0700

    final

--- public/client.js
+++ public/client.js
@@ -1,11 +1,10 @@
  $(function () {
  	$.get('/trucks', function (trucks) {
  		var truckList = [];
-		console.log(trucks);
- 	//	trucks.forEach(function (truck) {
- 	//		truckList.push('<li><a href="/trucks/' + truck.name + '/">' + truck.name + '</a></li>')
- 	//	});
- 	//	$('.truck-list').append(truckList);
-		$('.truck-list').append(trucks);
+		var truckObject = JSON.parse(trucks);
+ 		truckObject.forEach(function (truck) {
+ 			truckList.push('<li><a href="/trucks/' + truck.name + '/">' + truck.name + '</a></li>')
+ 		});
+ 		$('.truck-list').append(truckList);
  	});
  });

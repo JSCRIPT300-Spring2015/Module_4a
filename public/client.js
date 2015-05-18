8f6f87d57d9b5b432b75ef1618024290055956ca
Karen Nelson <karen.mc.nelson@gmail.com>
2015-05-17 19:38:02 -0700

    First commit of finished 4a assigment.

--- public/client.js
+++ public/client.js
@@ -1,9 +1,9 @@
-// $(function () {
-// 	$.get('/trucks', function (trucks) {
-// 		var truckList = [];
-// 		trucks.forEach(function (truck) {
-// 			truckList.push('<li><a href="/trucks/' + truck.name + '/">' + truck.name + '</a></li>')
-// 		});
-// 		$('.truck-list').append(truckList);
-// 	});
-// });
+ $(function () {
+	$.get('/trucks', function (trucks) {
+ 		var truckList = [];
+ 		trucks.forEach(function (truck) {
+ 			truckList.push('<li><a href="/trucks/' + truck.name + '/">' + truck.name + '</a></li>');
+ 		});
+ 		$('.truck-list').append(truckList);
+ 	});
+ });
